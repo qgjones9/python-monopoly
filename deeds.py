a6323b4a227d595284c7e41bd2d812555f262504
qgjones9 <38169271+qgjones9@users.noreply.github.com>
2018-06-02 06:28:20 -0700

    Add files via upload

---
+++ deeds.py
@@ -0,0 +1,12 @@
+class deeds:
+    def __init__(self, name, rent, rent_color_set, hideout, facility, mortgage, unmortgage):
+        self.name = name
+        self.rent = rent
+        self.rent_color_set = rent_color_set
+        self.hideout = hideout
+        self.facility = facility
+        self.mortgage = mortgage
+        self.unmortgage = unmortgage
+
+
+
---
+++ main.py
@@ -0,0 +1,17 @@
+import deeds
+
+def player1():
+    name = str(input("Player 1: Choose a player name: "))
+    return name
+
+
+def player2():
+    name = str(input("Player 2: Choose a player name: "))
+    return name
+
+
+first_player = player1()
+second_player = player2()
+print(first_player + " roll the dice to determine who goes first")
+print(first_player + " it is you turn to roll the dice to see who wins")
+
